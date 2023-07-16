d72d44b5a2a6eb639157118abd9424af327548ed
Castro <igordc38@gmail.com>
2023-07-16 02:57:22 -0300

    ICA [feature] finance repository

--- business/service/interfaces/IFinanceService.ts
+++ business/service/interfaces/IFinanceService.ts
@@ -3,7 +3,6 @@ import { FinanceModel } from "../../domain/entities/FinanceModel";
 export interface IFinanceService {
   createNewFinance(newFinance: FinanceModel): boolean;
   getFinanceById(financeId: string): FinanceModel | null;
-  getFinancesByUser(): FinanceModel[];
   getActiveFinances(): FinanceModel[];
   getDeletedFinances(): FinanceModel[];
   editFinance(finance: FinanceModel): boolean;
